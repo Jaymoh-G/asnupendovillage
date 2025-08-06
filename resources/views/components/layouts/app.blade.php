4e5425b935b476fbe5b7acacc681f9231a160dc8
Jaymoh-G <gmorembatia@gmail.com>
2025-08-06 17:41:27 +0300

    assets

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,23 +24,29 @@
 
 
     <!-- Jquery -->
-        <script src="/assets/js/vendor/jquery-3.7.1.min.js"></script>
+        <script src="{{
+                asset('assets/js/vendor/jquery-3.7.1.min.js')
+            }}"></script>
         <!-- Swiper Js -->
-        <script src="/assets/js/swiper-bundle.min.js"></script>
+        <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
         <!-- Bootstrap -->
-        <script src="/assets/js/bootstrap.min.js"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
         <!-- Magnific Popup -->
-        <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="{{
+                asset('assets/js/jquery.magnific-popup.min.js')
+            }}"></script>
         <!-- Counter Up -->
-        <script src="/assets/js/jquery.counterup.min.js"></script>
+        <script src="{{ asset('assets/js/jquery.counterup.min.js') }}"></script>
         <!-- Range Slider -->
-        <script src="/assets/js/jquery-ui.min.js"></script>
+        <script src="{{ asset('assets/js/jquery-ui.min.js') }}"></script>
         <!-- Isotope Filter -->
-        <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
-        <script src="/assets/js/isotope.pkgd.min.js"></script>
+        <script src="{{
+                asset('assets/js/imagesloaded.pkgd.min.js')
+            }}"></script>
+        <script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
 
         <!-- Main Js File -->
-        <script src="/assets/js/main.js"></script>
+        <script src="{{ asset('assets/js/main.js') }}"></script>
 
         @livewireScripts
     </body>
