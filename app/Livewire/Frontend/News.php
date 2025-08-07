c83665b801cc1b248e7309eb0a8def35d2e3513a
Jaymoh-G <gmorembatia@gmail.com>
2025-08-07 15:45:07 +0300

    pagebanner error

--- app/Livewire/Frontend/News.php
+++ app/Livewire/Frontend/News.php
@@ -5,10 +5,19 @@ namespace App\Livewire\Frontend;
 use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\News as NewsModel;
+use App\Traits\HasPageBanner;
 
 class News extends Component
 {
-    use WithPagination;
+    use WithPagination, HasPageBanner;
+
+    public $pageBanner;
+
+    public function mount()
+    {
+        // Get page banner for news
+        $this->pageBanner = $this->getPageBanner('news');
+    }
 
     public function render()
     {
