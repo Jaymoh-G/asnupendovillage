19ab902a9cf64076147c024812cbe7ba58628284
Jaymoh-G <gmorembatia@gmail.com>
2025-08-18 13:42:01 +0300

    DB Seeder

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -87,11 +87,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->requiresAuthentication()
-            ->login()
-            ->registration()
-            ->passwordReset()
-            ->emailVerification();
+            ]);
     }
 }
